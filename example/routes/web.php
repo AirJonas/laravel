bd7049478db57a3cb3bbd60861cad3656bf68ada
AirJonas <77889765+AirJonas@users.noreply.github.com>
2025-01-30 10:00:16 +0100

    learned about classes and models in php laravel

--- example/routes/web.php
+++ example/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Job;
+
 
 Route::get('/', function () {
     return view('home');
@@ -8,48 +10,14 @@ Route::get('/', function () {
 
 Route::get('/jobs', function () {
     return view('jobs', [
-        'jobs' => [
-            [
-                'id' => 1,
-                'title' => 'Director',
-                'salary' => '$50,000'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Programmer',
-                'salary' => '$10,000'
-            ],
-            [
-                'id' => 3,
-                'title' => 'Teacher',
-                'salary' => '$40,000'
-            ],
-        ]
+        'jobs' => Job::all()
     ]);
 });
 
 Route::get('/jobs/{id}', function ($id) {
-    $jobs =[
-            [
-                'id' => 1,
-                'title' => 'Director',
-                'salary' => '$50,000'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Programmer',
-                'salary' => '$10,000'
-            ],
-            [
-                'id' => 3,
-                'title' => 'Teacher',
-                'salary' => '$40,000'
-            ],
-        ];
+    $job = Job::find($id);
 
-        $job = Arr::first($jobs, fn($job) => $job['id']==$id);
-        
-    return view('job' , ['job' => $job]);
+    return view('job', ['job' => $job]);
 });
 
 
