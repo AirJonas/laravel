7cd4039ef7f7283ab6d786376b4ea0c400aa02b7
AirJonas <77889765+AirJonas@users.noreply.github.com>
2025-02-04 10:55:54 +0100

    learned a bit about debugger of laravel and aboyt lazy loading

--- example/resources/views/jobs.blade.php
+++ example/resources/views/jobs.blade.php
@@ -2,11 +2,15 @@
     <x-slot:heading>
         Jobs Page
     </x-slot:heading>
-    <ul>
+    <div class="space-y-4">
         @foreach ($jobs as $job)
-            <a href="/jobs/{{ $job['id'] }}" class="text-blue-500 hover:underline">
-                <li><strong>{{ $job['title'] }}:</strong> Pays {{ $job['salary'] }} per year </li>
+            <a href="/jobs/{{ $job['id'] }}" class="block px-4 py-6 border border-gray-200">
+                <div class="text-sm font-bold text-blue-500 ">{{ $job->employer->name }}</div>
+                <div>
+                    <strong>{{ $job['title'] }}:</strong> Pays {{ $job['salary'] }} per year
+                </div>
+
             </a>
         @endforeach
-    </ul>
+    </div>
 </x-layout>
