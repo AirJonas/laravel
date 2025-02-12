129ecb670752dfde61fd126f3bce738a3a735979
AirJonas <77889765+AirJonas@users.noreply.github.com>
2025-02-12 08:51:45 +0100

    routig

--- example/routes/web.php
+++ example/routes/web.php
@@ -8,6 +8,7 @@ Route::get('/', function () {
     return view('home');
 });
 
+//Index
 Route::get('/jobs', function () {
     $jobs = Job::with('employer')->latest()->simplePaginate(3);
 
@@ -16,19 +17,24 @@ Route::get('/jobs', function () {
     ]);
 });
 
+//create
 Route::get('/jobs/create', function () {
     return view('jobs.create');
 });
 
-
+//show
 Route::get('/jobs/{id}', function ($id) {
     $job = Job::find($id);
 
     return view('jobs.show', ['job' => $job]);
 });
 
+//Store
 Route::post('/jobs', function(){
-    // validation ..
+    request()->validate([
+        'title' => ['required', 'min:3'],
+        'salary' => ['required'],
+    ]);
 
     Job::create([
         'title' => request('title'),
@@ -39,6 +45,40 @@ Route::post('/jobs', function(){
     return redirect('/jobs');
 });
 
+//edit
+Route::get('/jobs/{id}/edit', function ($id) {
+    $job = Job::find($id);
+
+    return view('jobs.edit', ['job' => $job]);
+});
+
+//Update
+Route::patch('/jobs/{id}', function ($id) {
+    request()->validate([
+        'title' => ['required', 'min:3'],
+        'salary' => ['required'],
+    ]);
+
+    $job = Job::findOrFail($id);
+
+    $job->update([
+        'title' => request('title'),
+        'salary'=> request('salary'),
+    ]);
+
+    return redirect('/jobs/'. $job->id);
+
+
+});
+
+//Destroy
+Route::delete('/jobs/{id}', function ($id) {
+    $job = Job::findOrFail($id);
+    $job->delete();
+
+    return redirect('/jobs');
+
+});
 
 Route::get('/contact', function () {
     return view('contact');
