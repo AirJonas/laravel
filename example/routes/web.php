f538a4866c56a153be5e167fcfae4efcf6baf3d7
AirJonas <77889765+AirJonas@users.noreply.github.com>
2025-02-18 12:57:17 +0100

    learned about ques and workers

--- example/routes/web.php
+++ example/routes/web.php
@@ -3,10 +3,18 @@
 use App\Http\Controllers\JobController;
 use App\Http\Controllers\RegisteredUserController;
 use App\Http\Controllers\SessionController;
+use App\Jobs\TranslateJob;
 use Illuminate\Support\Facades\Route;
 use App\Models\Job;
 use Illuminate\Support\Facades\Mail;
 
+Route::get('test', function(){
+    $job = Job::first();
+    
+    TranslateJob::dispatch($job);
+
+    return 'done';
+});
 
 
 Route::get('/', function () {
